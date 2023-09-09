b45bb1ed561b9caca79ac983111d71aaae4ffbac
gusolra <51713522+gusolra@users.noreply.github.com>
2023-09-09 17:55:45 +0200

    Codigo inservible

--- src/main/java/org/example/HolaMundo.java
+++ src/main/java/org/example/HolaMundo.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class HolaMundo {
     public static void main(String[] args) {
+        // Comentario
         Mensaje msg = new Mensaje();
         String nombre = msg.solicitarDatos("Escribe tu nombre: ");
         msg.saludar(nombre);
