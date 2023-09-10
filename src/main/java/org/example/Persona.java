50caa930991e7eca02c42ede13dd5a848f9d1463
gusolra <gusolra@yahoo.es>
2023-09-10 21:24:51 +0200

    Cambio clase Persona

---
+++ src/main/java/org/example/Persona.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Persona {
+    String nombre, apellidos;
+
+    public Persona(){
+    }
+
+    public Persona(String nombre, String apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ' ' +apellidos;
+    }
+}
