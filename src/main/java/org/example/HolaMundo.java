f7f7fc48be23a36a640f39814c9f7aa3c9c93adb
gusolra <gusolra@yahoo.es>
2023-09-10 21:37:22 +0200

    Cambio clase HolaMundo - Mergeo

--- src/main/java/org/example/HolaMundo.java
+++ src/main/java/org/example/HolaMundo.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        // Comentario a eliminar 
-        System.out.println(">>");
         Mensaje msg = new Mensaje();
         Persona p1 = new Persona();
         p1.nombre = msg.solicitarDatos("Escribe tu nombre: ");
